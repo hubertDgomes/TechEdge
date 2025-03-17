10ad127620fca09aea579507a1adc9d3c97fd07c
Your Name <you@example.com>
2025-03-17 16:42:58 +0600

    Add initial slider styles and image assets

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
 new VenoBox({
     selector: '.my-video-links',
 });
+
+$('.sliders').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '30px',
+            slidesToShow: 1
+          }
+        },
+      ]
+  });
+
+  $('.sliderJS').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 0,
+    speed: 5000,
+    cssEase: 'linear',
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+        }
+      },
+      ]
+    });
+
